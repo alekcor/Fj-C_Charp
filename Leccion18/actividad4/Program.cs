7644d2dc65c89e6d382a898ab2f472b6dedc21b4
alekcor <89666868+alekcor@users.noreply.github.com>
2021-09-04 04:05:23 +0200

    add act3 and act4

---
+++ Leccion18/actividad4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace actividad4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este es mi segundo programa\nBienvenido al mundo de la programación");
+        }
+    }
+}
