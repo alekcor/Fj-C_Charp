a70da3ae40ecdabe4dadc13660beba878350d2e2
alekcor <89666868+alekcor@users.noreply.github.com>
2021-09-08 06:34:15 +0200

    updated actividad3

---
+++ Leccion18/actividad3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace actividad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo, soy Johan!!");
+        }
+    }
+}
