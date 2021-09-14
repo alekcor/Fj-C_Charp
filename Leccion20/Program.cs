05314866ab2c0a2fb30e1d33c064cb2373a04ea2
alekcor <89666868+alekcor@users.noreply.github.com>
2021-09-14 03:35:20 +0200

    add lession20

---
+++ Leccion20/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Leccion20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            Console.WriteLine("Introduzca un número");
+            num = int.Parse(Console.ReadLine());
+            if(num <0)
+            {
+                Console.WriteLine("El número es menor a cero");
+            }
+            else{
+                Console.WriteLine("El numero es igual o mayor a cero");
+            }
+        }
+    }
+}
